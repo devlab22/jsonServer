cedfd08217efda6918ee7b484ed72fd65a4cb4f4
VENGELHARD <v.engelhard@lighthouse-mcs.de>
2022-12-21 11:59:23 +0100

    config

--- index.js
+++ index.js
@@ -1,18 +1,31 @@
-const jsonServer = require('json-server')
-const cors = require('cors')
-const path = require('path')
+const jsonServer = require('json-server');
+const cors = require('cors');
+const path = require('path');
+const fs = require('fs');
+const dirname = process.cwd();
 
-const server = jsonServer.create()
-const router = jsonServer.router(path.join(__dirname, 'db.json'))
-const middlewares = jsonServer.defaults()
+const server = jsonServer.create();
+const router = jsonServer.router(path.join(dirname, 'db.json'));
+const middlewares = jsonServer.defaults();
 
-server.use(cors())
-server.use(jsonServer.bodyParser)
-server.use(middlewares)
-server.use(router)
+server.use(cors());
+server.use(jsonServer.bodyParser);
+server.use(middlewares);
+server.use(router);
 
-const PORT = 3001
+let config = {}
+
+try {
+  const rawdata = fs.readFileSync(path.join(dirname, 'config.json'));
+  const data = JSON.parse(rawdata);
+  config = data;
+}
+catch (e) {
+  
+}
+
+const PORT = config.PORT || 3001;
 
 server.listen(PORT, () => {
-  console.log(`JSON Server is running on http://localhost:${PORT}`)
+  console.log(`JSON Server is running on http://localhost:${PORT}`);
 })
